4b39c9f8f632be25302f3b64581f7c8851c1debf
unknown <1779742872@qq.com>
2025-12-21 18:17:38 +0800

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig(({ mode }) => {
   // @ts-expect-error process is a node global
   const env = loadEnv(mode, process.cwd(), '')
-  const target = env.VITE_API_BASE_URL || 'http://183.173.141.217:8000'
+  const target = env.VITE_API_BASE_URL || 'http://localhost:8000'
 
   return {
     plugins: [
